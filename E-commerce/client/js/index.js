51dbd7c5e4d2c235b704413cdb0d2a9ebf0308fa
Emmanuel <pc-500@live.com.ar>
2023-10-02 19:03:08 -0300

    .2/10
    .

--- E-commerce/client/js/index.js
+++ E-commerce/client/js/index.js
@@ -1,6 +1,9 @@
 const shopContent = document.getElementById("shopContent");
+const shopContent2 = document.getElementById("shopContent2");
 const cart = [];
 
+
+// primera seccion
 productos.forEach((product) => {
     const content = document.createElement("div");
     content.className = "card";
@@ -16,12 +19,12 @@ productos.forEach((product) => {
 
     content.append(buyButton);
 
-    buyButton.addEventListener("click", ()=>{
+    buyButton.addEventListener("click", () => {
         const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
-        
+
         if (repeat) {
-            cart.map((prod)=> {
-                if(prod.id == product.id){
+            cart.map((prod) => {
+                if (prod.id == product.id) {
                     prod.quanty++;
                     displayCartCounter();
                 }
@@ -36,5 +39,100 @@ productos.forEach((product) => {
             });
             displayCartCounter();
         }
+
     });
 });
+
+// segunda seccion
+productos2.forEach((product) => {
+    const content2 = document.createElement("div");
+    content2.className = "card2";
+    content2.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p class="price">${product.price} $</p>
+    `;
+    shopContent2.append(content2);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    content2.append(buyButton);
+
+    buyButton.addEventListener("click", () => {
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if (repeat) {
+            cart.map((prod) => {
+                if (prod.id == product.id) {
+                    prod.quanty++;
+                    displayCartCounter();
+                }
+            });
+        } else {
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+            displayCartCounter();
+        }
+
+    });
+});
+   
+// tercera seccion
+productos3.forEach((product) => {
+    const content3 = document.createElement("div");
+    content3.className = "card2";
+    content3.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p class="price">${product.price} $</p>
+    `;
+    shopContent3.append(content3);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    content3.append(buyButton);
+
+    buyButton.addEventListener("click", () => {
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if (repeat) {
+            cart.map((prod) => {
+                if (prod.id == product.id) {
+                    prod.quanty++;
+                    displayCartCounter();
+                }
+            });
+        } else {
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+            displayCartCounter();
+        }
+
+    });
+});
+
+console.log("cart is = ")
+console.log(cart);
+
+
+
+            
+    
+
+
+           
+
+
+   
