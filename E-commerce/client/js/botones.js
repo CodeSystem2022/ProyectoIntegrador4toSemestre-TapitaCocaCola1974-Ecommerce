2f09600522177fb7c29183f12f65af600966d928
Joaquin Avaro Acosta <95146374+joakko18@users.noreply.github.com>
2023-10-12 02:03:26 +0300

    Add files via upload

--- E-commerce/client/js/botones.js
+++ E-commerce/client/js/botones.js
@@ -1,39 +1,30 @@
- // Get references to the section and the button
- const section = document.getElementById("section1")
- const toggleSectionBtn = document.getElementById('toggleSectionBtn');
+// Function to toggle the visibility of a section
+function toggleSection(section) {
+    section.classList.toggle('hidden');
+}
 
- // Add a click event listener to the button
- toggleSectionBtn.addEventListener('click', function () {
-     // Toggle the 'hidden' class on the section
-     const isHidden = section.classList.toggle('hidden');
-     
-     // Change button text based on the section's visibility
-     toggleSectionBtn.innerText = isHidden ? 'Show Section' : 'Hide Section';
- });
+// Get references to the sections and the buttons
+const section1 = document.getElementById("section1");
+const toggleSectionBtn1 = document.getElementById('toggleSectionBtn');
 
-  // Get references to the section and the button
-  const section2 = document.getElementById("section2")
-  const toggleSectionBtn2 = document.getElementById('toggleSectionBtn2');
- 
-  // Add a click event listener to the button
-  toggleSectionBtn2.addEventListener('click', function () {
-      // Toggle the 'hidden' class on the section
-      const isHidden = section2.classList.toggle('hidden');
-      
-      // Change button text based on the section's visibility
-      toggleSectionBtn2.innerText = isHidden ? 'Show Section' : 'Hide Section';
-  });
+const section2 = document.getElementById("section2");
+const toggleSectionBtn2 = document.getElementById('toggleSectionBtn2');
 
-  //section3
+const section3 = document.getElementById("section3");
+const toggleSectionBtn3 = document.getElementById('toggleSectionBtn3');
 
-  const section3 = document.getElementById("section3")
-  const toggleSectionBtn3 = document.getElementById('toggleSectionBtn3');
- 
-  // Add a click event listener to the button
-  toggleSectionBtn3.addEventListener('click', function () {
-      // Toggle the 'hidden' class on the section
-      const isHidden = section3.classList.toggle('hidden');
-      
-      // Change button text based on the section's visibility
-      toggleSectionBtn3.innerText = isHidden ? 'Show Section' : 'Hide Section';
-  });
+// Add click event listeners to the buttons
+toggleSectionBtn1.addEventListener('click', function () {
+    console.log('Button 1 clicked');
+    toggleSection(section1);
+});
+
+toggleSectionBtn2.addEventListener('click', function () {
+    console.log('Button 2 clicked');
+    toggleSection(section2);
+});
+
+toggleSectionBtn3.addEventListener('click', function () {
+    console.log('Button 3 clicked');
+    toggleSection(section3);
+});
